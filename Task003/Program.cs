ef47d1c4eb9142f6b8d02b55bf09b08751d9d6bc
ELENA <battilena@gmail.com>
2022-08-25 19:45:32 +0300

    Task3

---
+++ Task003/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Является ли введённое число чётным, введите целое число x = ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if (x%2==0)
+{
+    Console.Write("Да, четное");
+}
+else
+{
+    Console.Write("нет, не четное");
+}
