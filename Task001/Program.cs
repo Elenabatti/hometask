63a2926a27e06125d15e44b379cf0c4784127e52
ELENA <battilena@gmail.com>
2022-08-25 17:49:54 +0300

    Task001

---
+++ Task001/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+Console.Write("введите число: ");
+int number1 = int.Parse(Console.ReadLine());
+Console.Write("введите число: ");
+int number2 = int.Parse(Console.ReadLine());
+if (number1 > number2)
+{
+    Console.WriteLine("max = " + number1);
+}
+else
+{
+    Console.WriteLine("max = " + number2);
+
+}
+if (number1 < number2)
+{
+    Console.WriteLine("min = " + number1);
+}
+else
+{
+    Console.WriteLine("min = " + number2);
+}
